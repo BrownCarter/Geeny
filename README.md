f8efef807d6592f3fd870de2352d520fcf544ce2
BrownCarter <lilbrown3000@gmail.com>
2020-04-21 16:38:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#READ ME FILE
+This is a simple readme file.
---
+++ TestApp.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	vector<int> myVector = {1, 3, 5, 2, 7, 9, 11 ,3, 14};
+	std::cout<<myVector<<"\n";
+	return 0;
+}
+
